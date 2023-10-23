0b615eb77f902628db711b163ba73fe5440714c9
Natanael Rolim <natanael.10@hotmail.com>
2023-10-22 22:36:57 -0300

    implementação da camada de dominio

---
+++ src/main/java/me/dio/domain/model/User.java
@@ -0,0 +1,32 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 150)
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Account account;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Card card;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Feature> features;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<News> news;
+
+
+}
